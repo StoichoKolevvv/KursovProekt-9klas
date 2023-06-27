7424341ff1f5e6ee02b2d208d90647383b398e00
Stoicho Kolevvv <122405390+StoichoKolevvv@users.noreply.github.com>
2023-06-28 00:56:59 +0300

    Add files via upload

---
+++ KursovProekt-9klas/Parts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarParts
+{
+    internal class Parts
+    {
+
+        public string Type { get; set; }
+        public string CarBrand { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Unit { get; set; }
+
+
+        public override string  ToString()
+        {
+             string c = $"Type: {Type} | \b, Car Brand: {CarBrand} | \b, Quantity: {Quantity} | \b, Unit Price: {UnitPrice} | \b , Unit: {Unit} |";
+            return c ;
+        }
+        
+
+
+
+
+    }
+}
